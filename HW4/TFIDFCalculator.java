93a3116671232d5d5462082f5e6329920744f345
rkkkloh <ruikangloh@gmail.com>
2024-05-21 22:38:08 +0800

    Implement method for each task

--- HW4/TFIDFCalculator.java
+++ HW4/TFIDFCalculator.java
@@ -1,8 +1,11 @@
+//package HW4;
+
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.io.FileWriter;
 
 public class TFIDFCalculator {
     public static void main(String args[]) {
@@ -12,6 +15,14 @@ public class TFIDFCalculator {
         String[] stringArgumentArray = null;
         String[] documentIndexArray = null;
         List<String> documenList = new ArrayList<>();
+
+	    try {
+            FileWriter writer = new FileWriter("output.txt");
+            writer.write("");
+            writer.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
         // handle input arguments
         try {
@@ -33,7 +44,7 @@ public class TFIDFCalculator {
 
         // grouping content into a document
         try {
-            BufferedReader reader= new BufferedReader(new FileReader(args[0]));
+            BufferedReader reader= new BufferedReader(new FileReader("docs.txt"));
             int lineCount = 0;
             String line;
             String documentContent = "";
@@ -45,16 +56,15 @@ public class TFIDFCalculator {
 
                 if (lineCount%5 == 0 && lineCount != 0) {
                     documenList.add(documentContent);
-
-		        documentContent = "";
+                    documentContent = "";
                 }
 
                 documentContent += line + " ";
                 lineCount++;
+
+                
             }
-            if (!documentContent.isEmpty()) {
-                    documenList.add(documentContent);
-            }
+            documenList.add(documentContent);
             reader.close();
 
         } catch (IOException e) {
@@ -68,16 +78,72 @@ public class TFIDFCalculator {
             String[] documentContent = documenList.get(Integer.parseInt(documentIndexArray[i])).split(" ");
             documentContentArrayList.add(documentContent);
         }
-        
-        TrieNode root = new TrieNode();
 
-        for (int i = 0; i < documentContentArrayList.size(); i++){
-            for (int j = 0; j < documentContentArrayList.get(i).length; j++) {
-                insert(documentContentArrayList.get(i)[j],root);
-            }
-            
+        List<String[]> idfDocumentContentArrayList = new ArrayList<>();
+        for (int i = 0; i < documenList.size(); i++) {
+            String[] documentContent = documenList.get(i).split(" ");
+            idfDocumentContentArrayList.add(documentContent);
+        }
+
+        TrieNode wholeDocRoot;
+        List<TrieNode> idfRoot = new ArrayList<>();
+
+        for (int i = 0; i < idfDocumentContentArrayList.size(); i++) {
+            wholeDocRoot = new TrieNode();
+            for (String word : idfDocumentContentArrayList.get(i))
+                insert(word,wholeDocRoot);
+            idfRoot.add(wholeDocRoot);
         }
         
+        TrieNode root;
+        int termCount = 0;
+        double termFrequency;
+        double idfValue;
+        double tfIdfValue;
+
+        try {
+            for (int i = 0; i < documentContentArrayList.size(); i++){
+                root = new TrieNode();
+
+                for (int j = 0; j < documentContentArrayList.get(i).length; j++) {
+                    insert(documentContentArrayList.get(i)[j],root);
+                }
+                
+                termCount = getTermCount(stringArgumentArray[i],root);
+
+                termFrequency = tf(termCount,documentContentArrayList.get(i).length);
+
+                idfValue = idf(idfRoot,stringArgumentArray[i],documenList.size());
+
+                tfIdfValue = termFrequency * idfValue;
+
+                fileOutput(tfIdfValue);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        
+    }
+
+    public static double tf(int number_term_in_doc,double documentSize) {        
+        return number_term_in_doc / documentSize;
+    }
+
+    public static double idf(List<TrieNode> idfRoot, String term,double documentsSize) {
+        int count = 0;
+        for (TrieNode root : idfRoot) {
+            if (search(term,root)) {
+                count++;
+            }
+        }
+        
+        return Math.log(documentsSize / count);
+    }
+    
+    public static void fileOutput(double tfIdfValue) throws IOException {
+        FileWriter writer = new FileWriter("output.txt",true);
+        writer.write(String.format("%.5f", tfIdfValue) + " ");
+        writer.close();
     }
 
     public static void insert(String word, TrieNode root) {
@@ -89,9 +155,10 @@ public class TFIDFCalculator {
             node = node.children[c - 'a'];
         }
         node.isEndOfWord = true;
+        node.count++;
     }
 
-    public boolean search(String word, TrieNode root) {
+    public static boolean search(String word, TrieNode root) {
         TrieNode node = root;
         for (char c : word.toCharArray()) {
             node = node.children[c - 'a'];
@@ -100,36 +167,23 @@ public class TFIDFCalculator {
             }
         }
         return node.isEndOfWord;
+    }
+
+    public static int getTermCount(String word, TrieNode root) {
+        TrieNode node = root;
+        for (char c : word.toCharArray()) {
+            node = node.children[c - 'a'];
+            if (node == null) {
+                return 0;
+            }
+        }
+        return node.count;
     }
 }
 
 class TrieNode {
     TrieNode[] children = new TrieNode[26];
     boolean isEndOfWord = false;
+    int count = 0;
 }
 
-class Trie {
-    TrieNode root = new TrieNode();
-
-    public void insert(String word) {
-        TrieNode node = root;
-        for (char c : word.toCharArray()) {
-            if (node.children[c - 'a'] == null) {
-                node.children[c - 'a'] = new TrieNode();
-            }
-            node = node.children[c - 'a'];
-        }
-        node.isEndOfWord = true;
-    }
-
-    public boolean search(String word) {
-        TrieNode node = root;
-        for (char c : word.toCharArray()) {
-            node = node.children[c - 'a'];
-            if (node == null) {
-                return false;
-            }
-        }
-        return node.isEndOfWord;
-    }
-}
